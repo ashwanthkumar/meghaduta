6caa01a0afbf3fbbbd667b2503ffc18206105a8b
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2015-09-19 19:36:58 +0530

    Integrating FileWatcher to LocalFileSpout

--- src/main/java/meghaduta/storm/spouts/LocalFileSpout.java
+++ src/main/java/meghaduta/storm/spouts/LocalFileSpout.java
@@ -5,11 +5,25 @@ import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.base.BaseRichSpout;
 import backtype.storm.tuple.Fields;
+import in.ashwanthkumar.utils.collections.Lists;
+import meghaduta.handlers.FileWatcher;
+import meghaduta.handlers.NewFileCreatedHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.util.Map;
 
 public class LocalFileSpout extends BaseRichSpout {
+    private static final Logger LOG = LoggerFactory.getLogger(LocalFileSpout.class);
+
     private SpoutOutputCollector collector;
+    private FileWatcher watcher;
+    private String directoryToWatch;
+
+    public LocalFileSpout(String directoryToWatch) {
+        this.directoryToWatch = directoryToWatch;
+    }
 
     @Override
     public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
@@ -19,9 +33,21 @@ public class LocalFileSpout extends BaseRichSpout {
     @Override
     public void open(Map map, TopologyContext topologyContext, SpoutOutputCollector spoutOutputCollector) {
         this.collector = spoutOutputCollector;
+        try {
+            watcher = new FileWatcher(new NewFileCreatedHandler() {
+                @Override
+                public void handle(File file) {
+                    collector.emit(Lists.<Object>of(file.getAbsolutePath()));
+                }
+            });
+            watcher.start(new File(directoryToWatch));
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+        }
     }
 
     @Override
     public void nextTuple() {
+        watcher.handle();
     }
 }
